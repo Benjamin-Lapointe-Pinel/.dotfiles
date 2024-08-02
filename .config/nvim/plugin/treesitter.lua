1009c1272f48155182259f63f5ef091a549ff54c
Benjamin Lapointe-Pinel <benjamin.lapointe13@gmail.com>
2024-08-02 12:12:53 -0400

    nvim treesitter incremental selection

--- .config/nvim/plugin/treesitter.lua
+++ .config/nvim/plugin/treesitter.lua
@@ -17,6 +17,22 @@ require'nvim-treesitter.configs'.setup{
 		'yaml',
 	},
 
+	highlight = {
+		enable = true
+	},
+
+	indent = {
+		enable = true
+	},
+
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			node_incremental = "v",
+			node_decremental = "V",
+		},
+	},
+
 	textobjects = {
 
 		select = {

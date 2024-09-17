5d6123dcdc30bb142388936e9ae32703a493dc79
Benjamin Lapointe-Pinel <benjamin.lapointe13@gmail.com>
2024-09-17 10:26:11 -0400

    nvim lsp disagnostic border single

--- .config/nvim/plugin/lsp.lua
+++ .config/nvim/plugin/lsp.lua
@@ -7,6 +7,7 @@ vim.api.nvim_set_hl(0, "DiagnosticSignError", { ctermbg=18, ctermfg=9 })
 require('lspconfig.ui.windows').default_options.border = 'single'
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "single" })
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "single" })
+vim.diagnostic.config { float = { border = "single" } }
 
 on_attach = function(client, bufnr)
 	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
